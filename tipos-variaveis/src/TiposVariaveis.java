176d6ec994643ee429128b3da228af09c3f0aac5
Bruno Mota <bmotadev@gmail.com>
2024-08-02 17:43:20 -0300

    tipos e variaveis 4

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,9 +1,27 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+        // Tipos primitivos
+        // classe String que representa texto na aplicação
+
+        String meuNome = "BRUNO MOTA";
+
+
         double salarioMinimo = 2500.33;
 
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+        
+
+        // final não permite a mudança de valor
+        final double VALOR_DE_PI = 3.14;
+        VALOR_DE_PI = 10.75;
+
     }
 }
