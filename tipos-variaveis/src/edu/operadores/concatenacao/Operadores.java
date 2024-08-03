0f14f0d755d44edfc0c73012fadc4182a9342c5e
Bruno Mota <bmotadev@gmail.com>
2024-08-03 14:45:06 -0300

    operadores unarios

--- tipos-variaveis/src/edu/operadores/concatenacao/Operadores.java
+++ tipos-variaveis/src/edu/operadores/concatenacao/Operadores.java
@@ -1,3 +1,4 @@
+package edu.operadores.concatenacao;
 public class Operadores {
 
     public static void main(String[] args) {
---
+++ tipos-variaveis/src/edu/operadores/ternario/Operadores.java
@@ -0,0 +1,19 @@
+package edu.operadores.ternario;
+
+public class Operadores {
+
+    public static void main(String[] args) {
+        int numero = 5;
+
+        numero = - numero;
+
+        // System.out.println(- numero);
+
+        System.out.println(numero);
+
+        numero = numero * -1; // torna o valor positivo
+
+        System.out.println(numero);
+    }
+
+}
