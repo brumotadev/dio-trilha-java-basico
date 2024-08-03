ff09b7130eb5c55f2ea4225d9adc70ade66979b1
Bruno Mota <bmotadev@gmail.com>
2024-08-03 17:30:33 -0300

    operadores unarios 2

---
+++ tipos-variaveis/src/edu/operadores/ternario2/Operadores.java
@@ -0,0 +1,23 @@
+package edu.operadores.ternario2;
+
+public class Operadores {
+
+    public static void main(String[] args) {
+        int numero =  5;
+
+        // numero = numero + 2;
+        numero++;
+
+        System.out.println(++numero);
+
+        System.out.println(numero);
+
+        numero--;
+        System.out.println(numero--);
+
+
+        boolean variavel = true;
+        System.out.println(!variavel); // nega o valor, vira false
+    }
+
+}
