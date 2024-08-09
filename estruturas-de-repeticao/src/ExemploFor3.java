f7f34ebf460c27cf31513e7015789388190001e0
Bruno Mota <bmotadev@gmail.com>
2024-08-08 21:54:14 -0300

    estruturas de repeticao em java - Exemplo For - Novas estruturas - Utilizando array

---
+++ estruturas-de-repeticao/src/ExemploFor3.java
@@ -0,0 +1,10 @@
+public class ExemploFor3 {
+
+    public static void main(String[] args) {
+        String alunos[] = { "BRUNO", "JESSICA", "ROSANGELA" };
+
+        for (int x = 0; x < alunos.length; x++) {
+            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+        }
+    }
+}
