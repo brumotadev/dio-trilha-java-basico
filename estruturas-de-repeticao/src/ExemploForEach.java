059da2aba475cb0f7c77336c9a054e84879c1136
Bruno Mota <bmotadev@gmail.com>
2024-08-08 22:17:29 -0300

    estruturas de repeticao em java - Exemplo ForEach

---
+++ estruturas-de-repeticao/src/ExemploForEach.java
@@ -0,0 +1,12 @@
+public class ExemploForEach {
+
+    public static void main(String[] args) {
+        String alunos[] = { "BRUNO", "JESSICA", "ROSANGELA", "IRENE" };
+
+        // Forma abreviada
+        for (String aluno : alunos) {
+            System.out.println(aluno);
+        }
+
+    }
+}
