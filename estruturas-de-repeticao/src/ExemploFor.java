9bb86a9cfb31f710182e4dea34138010a7b414d4
Bruno Mota <bmotadev@gmail.com>
2024-08-08 21:50:07 -0300

    estruturas de repeticao em java - Exemplo For

---
+++ estruturas-de-repeticao/src/ExemploFor.java
@@ -0,0 +1,8 @@
+public class ExemploFor {
+public static void main(String[] args) {
+    // Exemplo: Contar até 20 carneirinhos, para pegar no sono:
+    for(int carneirinhos = 1 ; carneirinhos <=20; carneirinhos ++) {
+        System.out.println(carneirinhos + " - Carneirinho(s)");
+    }
+}
+}
