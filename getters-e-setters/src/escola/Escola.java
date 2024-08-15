c351edd609f5ced943db2571031c7742414b7019
Bruno Mota <bmotadev@gmail.com>
2024-08-14 21:37:12 -0300

    :recycle: refactor: adicionado sets para a instancia dos atributos do aluno em escola

--- getters-e-setters/src/escola/Escola.java
+++ getters-e-setters/src/escola/Escola.java
@@ -3,8 +3,8 @@ package escola;
 public class Escola {
     public static void main(String[] args) {
         Aluno bruno = new Aluno();
-        bruno.nome = "Bruno";
-        bruno.idade = 21;
+        bruno.setNome("Bruno");
+        bruno.setIdade(23);
 
         System.out.println("O Aluno " + bruno.nome + " tem " + bruno.idade + " anos ");
     }
