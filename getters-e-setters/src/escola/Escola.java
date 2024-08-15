5eb817350822b4ba96c05a67222ca6761710a3bd
Bruno Mota <bmotadev@gmail.com>
2024-08-14 21:18:52 -0300

    :sparkles: feat: Adicionado  atributo main na classe escola - Instanciado o objeto - Chamado seus atributos - Impressão dos dados

--- getters-e-setters/src/escola/Escola.java
+++ getters-e-setters/src/escola/Escola.java
@@ -1,5 +1,11 @@
 package escola;
 
 public class Escola {
+    public static void main(String[] args) {
+        Aluno bruno = new Aluno();
+        bruno.nome = "Bruno";
+        bruno.idade = 21;
 
+        System.out.println("O Aluno " + bruno.nome + " tem " + bruno.idade + " anos ");
+    }
 }
