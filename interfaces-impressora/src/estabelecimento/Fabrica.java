b5be66f7078133ed6463d9e6652c7565327a6878
Bruno Mota <bmotadev@gmail.com>
2024-08-18 15:17:20 -0300

    :sparkles: feat: utilizando interfaces

--- interfaces-impressora/src/estabelecimento/Fabrica.java
+++ interfaces-impressora/src/estabelecimento/Fabrica.java
@@ -1,7 +1,27 @@
 package estabelecimento;
 
+import equipamentos.copiadora.Copiadora;
+import equipamentos.digitalizadora.Digitalizadora;
+import equipamentos.digitalizadora.Scanner;
+import equipamentos.impressora.Deskjet;
+import equipamentos.impressora.Impressora;
+import equipamentos.multifuncional.EquipamentoMultifuncional;
+
 public class Fabrica {
     public static void main(String[] args) {
-        
+        EquipamentoMultifuncional equipamentoMultifuncional = new EquipamentoMultifuncional();
+
+        Scanner scanner = new Scanner();
+
+        Impressora impressora = equipamentoMultifuncional;
+        Digitalizadora digitalizadora = equipamentoMultifuncional;
+        Copiadora copiadora = equipamentoMultifuncional;
+
+        impressora.imprimir();
+        digitalizadora.digitalizar();
+        copiadora.copiar();
+
+
+
     }
 }
